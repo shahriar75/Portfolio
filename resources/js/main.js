7d251a7ee9f07e59b6c1a1c645afb9ec1a5ae616
shahriar75 <shahriaremon971@gmail.com>
2022-08-23 01:22:45 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+//Humberger Menu
+function openMenu(){
+    document.getElementById('menu-container').style.width="100%";
+}
+
+function closeMenu(){
+    document.getElementById('menu-container').style.width="0%";
+}
+
+// MIXITUP 
+var mixer = mixitup('.work-grid');
